6eb3212edce2bf571727d054bef46014514ff4df
MetinMemeti <metinmemeti13@gmail.com>
2025-01-18 17:55:46 +0100

    Starting the project

---
+++ libraryprojectjava1/src/main/java/com/example/libraryprojectjava1/Libraryprojectjava1Application.java
@@ -0,0 +1,13 @@
+package com.example.libraryprojectjava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libraryprojectjava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Libraryprojectjava1Application.class, args);
+	}
+
+}
