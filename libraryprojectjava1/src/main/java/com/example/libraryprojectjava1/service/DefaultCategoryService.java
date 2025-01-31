7177cac793973c49f0be52e7ce403ad5f3823f50
MetinMemeti <metinmemeti13@gmail.com>
2025-01-31 20:54:50 +0100

    Updating DefaultCategoryService

--- libraryprojectjava1/src/main/java/com/example/libraryprojectjava1/service/DefaultCategoryService.java
+++ libraryprojectjava1/src/main/java/com/example/libraryprojectjava1/service/DefaultCategoryService.java
@@ -1,4 +1,41 @@
 package com.example.libraryprojectjava1.service;
 
-public class DefaultCategoryService {
+import com.example.libraryprojectjava1.pojo.entity.Category;
+import com.example.libraryprojectjava1.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DefaultCategoryService implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Optional<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Optional<Category> getCategoryById(Integer id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void deleteCategory(Integer id) {
+        categoryRepository.deleteById(id);
+    }
 }
