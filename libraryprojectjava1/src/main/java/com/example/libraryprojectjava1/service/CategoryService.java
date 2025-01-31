050a7230dbf3947eaa57934ff7c867f42e862405
MetinMemeti <metinmemeti13@gmail.com>
2025-01-31 20:55:04 +0100

    Updating CategoryService

--- libraryprojectjava1/src/main/java/com/example/libraryprojectjava1/service/CategoryService.java
+++ libraryprojectjava1/src/main/java/com/example/libraryprojectjava1/service/CategoryService.java
@@ -2,10 +2,13 @@ package com.example.libraryprojectjava1.service;
 
 import com.example.libraryprojectjava1.pojo.entity.Category;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 public interface CategoryService {
     Optional<Category> getCategoryByName(String name);
     Category saveCategory(Category category);
     Optional<Category> getCategoryById(Integer id);
+    List<Category> getAllCategories();
+    void deleteCategory(Integer id);
 }
